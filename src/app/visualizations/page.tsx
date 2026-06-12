a5699e797305c74b23cca3dfae8cc24124c23d58
Roger Pelletier <swellpools@gmail.com>
2026-06-12 17:45:54 -0400

    Add visualizations page

---
+++ src/app/visualizations/page.tsx
@@ -0,0 +1,9 @@
+import ProtocolVisualizations from "@/components/ProtocolVisualizations";
+
+export default function VisualizationsPage() {
+  return (
+    <main className="min-h-screen bg-black text-white">
+      <ProtocolVisualizations />
+    </main>
+  );
+}
